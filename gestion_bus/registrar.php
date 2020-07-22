d3112966f4a79e795efaab684d6aec865fbeba80
Ivan Lopez <57471126+9ivan0@users.noreply.github.com>
2020-07-22 11:28:04 -0500

    registrar 2

---
+++ gestion_bus/registrar.php
@@ -0,0 +1,33 @@
+<div class="modal fade" id="insertarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Agregar nuevo registro</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <form id="frminsert" onsubmit="return insertarDatos()" method="post">
+              <label>Placa del Bus: </label>
+              <input type="text" id="placa" name="placa" class="form-control form-control-sm" required="">
+              <label>Marca del Bus: </label>
+              <input type="text" id="marca" name="marca" class="form-control form-control-sm" required="">
+              <label>Color del Bus: </label>
+              <input type="text" id="color" name="color" class="form-control form-control-sm" required="">
+              <label>Modelo del Bus: </label>
+              <input type="text" id="modelo" name="modelo" class="form-control form-control-sm" required="">
+              <label>Empresa del Bus: </label>
+              <input type="text" id="empresa" name="empresa" class="form-control form-control-sm" required="">
+             
+              <br>
+               <input type="submit" value="Guardar" class="btn btn-info">
+          </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+      </div>
+    </div>
+  </div>
+</div>
